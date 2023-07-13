e31519d143d86cdecc19a4ba4d906fea40baf1d6
Dylan Durand <dylan.durand88@outlook.fr>
2023-07-13 19:13:19 +0200

    Update modal

--- modal-hrnet/dist/ModalHrnet/ModalHrnet.js
+++ modal-hrnet/dist/ModalHrnet/ModalHrnet.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import './ModalHrnet.css';
 export default function ModalHrnet() {
-  return /*#__PURE__*/React.createElement("div", null, "Hello modale");
+  return /*#__PURE__*/React.createElement("div", {
+    className: "modal-container"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "modal-content"
+  }, /*#__PURE__*/React.createElement("p", null, "Your employee has been created success ! "), /*#__PURE__*/React.createElement("div", {
+    className: "modal-content__ctas flex"
+  }, /*#__PURE__*/React.createElement("button", {
+    className: "btn--plain btn--green"
+  }, "Return to board"), /*#__PURE__*/React.createElement("button", {
+    className: "btn--plain btn--green"
+  }, "Add a new employee"))));
 }
