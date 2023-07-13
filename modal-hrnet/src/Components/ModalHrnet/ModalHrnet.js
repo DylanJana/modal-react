1ab420adf5de59eb04aedae2a5ff258d73533ce0
Dylan Durand <dylan.durand88@outlook.fr>
2023-07-13 19:20:57 +0200

    Create modal condition

--- modal-hrnet/src/Components/ModalHrnet/ModalHrnet.js
+++ modal-hrnet/src/Components/ModalHrnet/ModalHrnet.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import './ModalHrnet.css'
 
-export default function ModalHrnet() {
-  return (
-    <div className='modal-container'>
-      <div className="modal-content">
-        <p>Your employee has been created success ! </p>
-        <div className="modal-content__ctas flex">
-          <button className='btn--plain btn--green'>Return to board</button>
-          <button className='btn--plain btn--green'>Add a new employee</button>
+export default function ModalHrnet({openModal}) {
+  if(openModal === true) {
+    console.log("FORM CORRECT")
+    return (
+      <div className='modal-container'>
+        <div className="modal-content">
+          <p>Your employee has been created success ! </p>
+          <div className="modal-content__ctas flex">
+            <button className='btn--plain btn--green'>Return to board</button>
+            <button className='btn--plain btn--green'>Add a new employee</button>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    console.log("FORM NOCOMPLETE")
+  }
 }
