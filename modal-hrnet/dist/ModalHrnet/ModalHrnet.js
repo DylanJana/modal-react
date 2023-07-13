2220f78144ee0047f5e252b000c760f3ed3359db
Dylan Durand <dylan.durand88@outlook.fr>
2023-07-13 19:21:48 +0200

    Update modal

--- modal-hrnet/dist/ModalHrnet/ModalHrnet.js
+++ modal-hrnet/dist/ModalHrnet/ModalHrnet.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import './ModalHrnet.css';
-export default function ModalHrnet() {
-  return /*#__PURE__*/React.createElement("div", {
-    className: "modal-container"
-  }, /*#__PURE__*/React.createElement("div", {
-    className: "modal-content"
-  }, /*#__PURE__*/React.createElement("p", null, "Your employee has been created success ! "), /*#__PURE__*/React.createElement("div", {
-    className: "modal-content__ctas flex"
-  }, /*#__PURE__*/React.createElement("button", {
-    className: "btn--plain btn--green"
-  }, "Return to board"), /*#__PURE__*/React.createElement("button", {
-    className: "btn--plain btn--green"
-  }, "Add a new employee"))));
+export default function ModalHrnet({
+  openModal
+}) {
+  if (openModal === true) {
+    console.log("FORM CORRECT");
+    return /*#__PURE__*/React.createElement("div", {
+      className: "modal-container"
+    }, /*#__PURE__*/React.createElement("div", {
+      className: "modal-content"
+    }, /*#__PURE__*/React.createElement("p", null, "Your employee has been created success ! "), /*#__PURE__*/React.createElement("div", {
+      className: "modal-content__ctas flex"
+    }, /*#__PURE__*/React.createElement("button", {
+      className: "btn--plain btn--green"
+    }, "Return to board"), /*#__PURE__*/React.createElement("button", {
+      className: "btn--plain btn--green"
+    }, "Add a new employee"))));
+  } else {
+    console.log("FORM NOCOMPLETE");
+  }
 }
