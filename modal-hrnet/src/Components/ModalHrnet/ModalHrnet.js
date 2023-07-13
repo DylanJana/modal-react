198f1c22dac646482550c83fafbc923046363c1a
Dylan Durand <dylan.durand88@outlook.fr>
2023-07-13 19:07:24 +0200

    Layout modal

--- modal-hrnet/src/Components/ModalHrnet/ModalHrnet.js
+++ modal-hrnet/src/Components/ModalHrnet/ModalHrnet.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import './ModalHrnet.css'
 
-export default function ModalHrnet({children}) {
+export default function ModalHrnet() {
   return (
-    <div>
-      {children}
+    <div className='modal-container'>
+      <div className="modal-content">
+        <p>Your employee has been created success ! </p>
+        <div className="modal-content__ctas flex">
+          <button className='btn--plain btn--green'>Return to board</button>
+          <button className='btn--plain btn--green'>Add a new employee</button>
+        </div>
+      </div>
     </div>
   )
 }
